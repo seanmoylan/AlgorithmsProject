1b34b2d007737470b7a8e45bc9cdb36d6b88380b
seanmoylan <sean.cheezy.moylan@gmail.com>
2020-04-07 13:11:01 +0100

    Functions moved as they were in wrong place. Created empty functions nexthash() and nextblock(). Also created a union block

--- MD5.c
+++ MD5.c
@@ -25,9 +25,58 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 // Initialize MD buffers that consistes of 4 32bit integers
 const uint32_t WORDS[] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
+
+
+// Functions (F, G, H, I)
+
+// --- F Function ---
+uint32_t F(uint32_t x, uint32_t y, uint32_t z){
+  return (x & y) | (~x & z);
+}
+
+
+// --- G Function ---
+uint32_t G(uint32_t x, uint32_t y, uint32_t z){
+  return (x & z) | (y & ~z);
+}
+
+
+// --- H Function ---
+uint32_t H(uint32_t x, uint32_t y, uint32_t z){
+  return (x ^ y ^ z);
+}
+
+// --- I Function ---
+uint32_t I(uint32_t x, uint32_t y, uint32_t z){
+  return (y ^ (x | ~z));
+}
+
+
+// Union allows you to store diffrent data types in the same memory address
+// this union is size 512bits that can be accessed as:
+// 8 x 64 bits
+// 16 x 32 bits
+// 64 x 8 bits
+union block {
+  uint64_t six_four[8];
+  uint32_t three_two[16];
+  uint8_t eight[64];
+};
+
+// Reads in the next block to be processed
+int nextblock(){
+
+}
+
+
+// Processes the next hash to be computed
+void nexthash(){
+
+}
 
 
 int main(int argc, char *argv[ ]) {
@@ -43,31 +92,5 @@ int main(int argc, char *argv[ ]) {
     printf("Error! Could not open file");
   }
 
-
-  // Functions (F, G, H, I)
-
-  // --- F Function ---
-  uint32_t F(uint32_t x, uint32_t y, uint32_t z){
-    return (x & y) | (~x & z);
-  }
-
-
-  // --- G Function ---
-  uint32_t G(uint32_t x, uint32_t y, uint32_t z){
-    return (x & z) | (y & ~z);
-  }
-
-
-  // --- H Function ---
-  uint32_t H(uint32_t x, uint32_t y, uint32_t z){
-    return (x ^ y ^ z);
-  }
-
-  // --- I Function ---
-  uint32_t I(uint32_t x, uint32_t y, uint32_t z){
-    return (y ^ (x | ~z));
-  }
-
-  
 
 }
